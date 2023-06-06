b9fb66ca590482e17621f80c169935243e8d374b
sai-praneeth8 <132859417+sai-praneeth8@users.noreply.github.com>
2023-06-06 06:19:45 +0000

    Pending changes exported from your codespace

--- materials/hello-world.py
+++ materials/hello-world.py
@@ -6,3 +6,23 @@ from gem5.components.processors.cpu_types import CPUTypes
 from gem5.resources.resource import obtain_resource
 from gem5.simulate.simulator import Simulator
 from gem5.isas import ISA
+
+# Obtain the components.
+cache_hierarchy = NoCache()
+memory = SingleChannelDDR3_1600("1GiB")
+processor = SimpleProcessor(cpu_type=CPUTypes.ATOMIC, num_cores=1, isa=ISA.X86)
+# Add them to the board.
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+
+# Obtain a binary to run via gem5-resources.
+binary = obtain_resource("x86-hello64-static")
+board.set_se_binary_workload(binary)
+# Setup the Simulator and run the simulation.
+simulator = Simulator(board=board)
+simulator.run()
